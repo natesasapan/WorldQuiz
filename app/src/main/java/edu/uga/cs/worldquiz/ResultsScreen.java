50f9c8f9a191f59260940ac32ae5da46c8beefc8
Regina Dong <rd88449@uga.edu>
2025-04-01 11:16:22 -0400

    home button on results page

--- app/src/main/java/edu/uga/cs/worldquiz/ResultsScreen.java
+++ app/src/main/java/edu/uga/cs/worldquiz/ResultsScreen.java
@@ -1,7 +1,11 @@
 package edu.uga.cs.worldquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultsScreen extends AppCompatActivity {
 
@@ -13,5 +17,17 @@ public class ResultsScreen extends AppCompatActivity {
         // This activity doesn't do anything yet - it just exists
 
 
+        // home button
+        Button homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // back to MainActivity
+                Intent intent = new Intent(ResultsScreen.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        }); // home button on click listener
     }
 }
